a5048f4dad54d67349236979d65a0af0707d1947
isti4github <isti.ru.cse@gmail.com>
2022-03-23 15:46:11 +0900

    erc721 added

---
+++ ERC721.sol
@@ -0,0 +1,25 @@
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT721 is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("IAT721", "IA721") {}
+
+    function CreateNFT(address owner, string memory tokenURI) public returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newid = _tokenIds.current();
+        _mint(owner, newid);
+        _setTokenURI(newid, tokenURI);
+
+        return newid;
+    }
+}
