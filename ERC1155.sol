47ea29bf01e07dec5122731b10d1e4ccb7aa289a
isti4github <isti.ru.cse@gmail.com>
2022-03-29 14:45:27 +0900

    ERC1155 added modified and image source discovered

--- ERC1155.sol
+++ ERC1155.sol
@@ -50,7 +50,8 @@ https://nft.storage/files/
 If you click on foles you will get the link of json that is your json file link.
 For the Images You will do the same. And get the link to put inside your Json.
 After running your smart contract. Just upload your contract to Mobile wallet NFT section by Token ID
-
+Imahe upload hints to following site
+https://stackoverflow.com/questions/69239521/unable-to-display-pokemon-image-from-pokeapi-co
 
 */
 //https://www.youtube.com/watch?v=19SSvs32m8I
